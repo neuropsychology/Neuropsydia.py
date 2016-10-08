a35aaabc78ea8c19334ec8c48ff3529a8a8dd5d4
DominiqueMakowski <dom.mak19@gmail.com>
2016-10-08 12:03:47 +0200

    0.0.2

--- neuropsydia/tests/test_color.py
+++ neuropsydia/tests/test_color.py
@@ -5,5 +5,5 @@ n.start(open_window=False)
 
 class TestColor(TestCase):
     def test_is_string(self):
-        s = funniest.joke()
-        self.assertTrue(isinstance(s, basestring))
+        c = n.color("w")
+        self.assertTrue(isinstance(c, tuple))
