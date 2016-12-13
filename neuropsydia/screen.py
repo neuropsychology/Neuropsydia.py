f2a69183aee799cca3b7efe5363f146e909c73ba
Dominique Makowski <dom.mak19@gmail.com>
2016-12-13 11:56:58 +0100

    modified:   neuropsydia/meta.py 	modified:   neuropsydia/screen.py

--- neuropsydia/screen.py
+++ neuropsydia/screen.py
@@ -2,10 +2,9 @@
 """
 Module initializing screen object and screen values.
 """
-
 import pygame
 
 screen = pygame.display.set_mode((0,0), pygame.SRCALPHA | pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE)
 screen_width, screen_height = screen.get_size()
 
-monitor_diagonal = 24 # inch
+monitor_diagonal = 24  # inch
