d949e128da8943c90bbc9ba50de4761340cfb6b9
DominiqueMakowski <dom.mak19@gmail.com>
2017-01-06 12:46:28 +0100

    dependencies fix

--- neuropsydia/__init__.py
+++ neuropsydia/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.0.0"
+__version__ = "1.0.3"
 
 from .path import *
 from .core import *
--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ package_data = {
 	"neuropsydia.files.binary":["*.png"],
 	"neuropsydia.files.logo":["*.png"]},
 install_requires = [
-    'os',
     'time',
     'datetime',
     'ctypes',
