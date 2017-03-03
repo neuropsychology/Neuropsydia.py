4a7636cac696eaae7ab7dc3485fc7918ddb21396
Dominique Makowski <dom.mak19@gmail.com>
2017-03-03 11:36:58 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,15 +28,8 @@ sys.modules['pygame.constants'] = Mock()
 
 sys.path.insert(0, os.path.abspath('../'))
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-MOCK_MODULES = ['pygame', 'numpy', 'pyxid', 'neurokit', 'pandas', 'Pillow']
-=======
-MOCK_MODULES = ['pygame', 'numpy', 'scipy', 'pandas', 'Pillow', 'plotly', 'cryptography', 'docx', 'python-docx', 'neurokit']
->>>>>>> origin/master
-=======
 MOCK_MODULES = ['pygame', 'numpy', 'pyxid', 'neurokit', 'pandas', 'Pillow', 'python-docx', 'cryptography']
->>>>>>> origin/dev
+
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
