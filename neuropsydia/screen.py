e05658789f9f9549243fc1169583743a05e5256a
DominiqueMakowski <dom.mak19@gmail.com>
2017-03-04 12:56:59 +0100

    window icon

--- neuropsydia/screen.py
+++ neuropsydia/screen.py
@@ -3,8 +3,18 @@
 Module initializing screen object and screen values.
 """
 import pygame
+from .path import *
 
+# Add icon to the window
+pygame.image.load(Path.logo() + 'N.png')
+
+# Create screen
 screen = pygame.display.set_mode((0,0), pygame.SRCALPHA | pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE)
+
+# Name the screen
+pygame.display.set_caption('Neuropsydia')
+
+# Get screen dimensions
 screen_width, screen_height = screen.get_size()
 
 monitor_diagonal = 24  # inch
