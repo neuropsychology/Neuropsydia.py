1b9d525e6e3d4077c78977c4de0760aa8ef1c058
DominiqueMakowski <dom.mak19@gmail.com>
2017-03-04 13:03:09 +0100

    window icon fix

--- neuropsydia/screen.py
+++ neuropsydia/screen.py
@@ -6,7 +6,7 @@ import pygame
 from .path import *
 
 # Add icon to the window
-pygame.image.load(Path.logo() + 'N.png')
+pygame.display.set_icon(pygame.image.load(Path.logo() + 'icon.png'))
 
 # Create screen
 screen = pygame.display.set_mode((0,0), pygame.SRCALPHA | pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.HWSURFACE)
