473e280f14fc09daacf472aa8a22d867a9c950e1
DominiqueMakowski <dom.mak19@gmail.com>
2017-05-01 12:53:22 +0200

    questionnaire docs

--- neuropsydia/meta.py
+++ neuropsydia/meta.py
@@ -87,6 +87,110 @@ def questionnaire(questions_dictionary, questions_list_key_name='Item', backgrou
 
     questions_list_key_name : str
         Key name of the sub-dict containing the items.
+    background : str
+        Background color.
+    size : int
+        Question's size.
+    show_page_number : bool
+        Show page number.
+    randomize : bool
+        Randomize question presentation.
+    reverse : bool
+        Needs a "Reverse" sub-dict with booleans showing which questions are reversed.
+    results_save : bool
+        Save the results.
+    results_name : str
+        Filename.
+    results_path : str
+        Path where to save.
+    participant_id : str
+        Append the participant's name in the filename.
+    dimensions_mean : bool
+        Compute the mean by dimension. Needs a "Dimension" sub-dict.
+    dimensions_key_name : str
+        Key name of the sub-dict containing dimensions.
+    style : str
+        style, check `neuropsydia.scale_styles()` function to see what's available.
+    x : float
+        Horizontal position of the center (from -10 (left) to 10 (right)).
+    y : float
+        Vertical position of the center (from -10 (bottom) to 10 (top)).
+    anchors : list of two str
+        a list of two propositions to be displayed on the sides of the scale (e.g., [not at all, very much]).
+    anchors_space : float
+        spacing betweeen the edge and the anchors.
+    anchors_size : float
+        size of the anchors' font.
+    edges : list
+        the underlying numerical edges of the scale.
+    validation : bool
+        confirm the response with a second left click or withdraw with a right click.
+    analog : bool
+        continuous (discrete) scale.
+    step : int
+        if analog is True, what are the step to go between the edges (determine the number of points on the scale).
+    labels : str or list
+        "num", "numeric" or "numbers" or list of actual text labels (e.g., ["not at all", "a bit", "very much"]).
+    labels_size : float
+        Size of labels.
+    labels_rotation : float
+        Labels rotation angle.
+    labels_space : float
+        Space between scale and labels.
+    labels_x : float
+        Horizontal dodging position.
+    line_thickness : float
+        Scale line thickness.
+    line_length : float
+        Scale line length.
+    line_color : str
+        Scale line color.
+    background : str
+        Scale background color.
+    title : str
+        Scale title/question to ask.
+    title_style : str
+        'body', 'psychometry', 'psychometry_bold', 'light', 'bold'. You can also insert the name of a system font, or the path to a specific font.
+    title_size : float
+        Title size.
+    title_space : float
+        Space between scale and title.
+    point_center : bool
+        Place a point at the center.
+    point_edges : bool
+        Place points at the edges.
+    reverse : bool
+        The result is scored as inverse.
+    force_separation : int
+        Creates visual separations with points.
+    separation_labels : list
+        Place labels corresponding to the separations.
+    separation_labels_size : float
+        Separation labels size.
+    separation_labels_rotate : float
+        Separation labels rotation angle.
+    separation_labels_space : float
+        Space between scale and separation labels.
+    show_result : bool
+        Add a marker to show the value on scale.
+    show_result_shape : str
+        Shape of this marker. "circle" or "rectangle".
+    show_result_shape_fill_color : str
+        Fill color of the marker.
+    show_result_shape_line_color : str
+        Line color of the marker.
+    show_result_shape_size : float
+        Marker's shape size.
+    show_result_space : float
+        Space between scale and marker.
+    show_result_size : float
+        Results text size.
+    show_result_color : str
+        Results text color.
+    show_result_decimals : int
+        How many decimals to show.
+    cursor_size : float
+        Size of the circle cursor.
 
     Returns
     ----------
